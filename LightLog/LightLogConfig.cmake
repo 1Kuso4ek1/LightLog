787cc2d88b9ed260b8dee16c0584b3820ce3fa28
Дмитрий <53074863+1Kuso4ek1@users.noreply.github.com>
2022-04-13 18:01:54 +0300

    Fixed Config file

--- LightLog/LightLogConfig.cmake
+++ LightLog/LightLogConfig.cmake
@@ -1,2 +1,2 @@
 get_filename_component(LIGHTLOG_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-get_filename_component(LIGHTLOG_INCLUDE_DIR "${LIGHTLOG_DIR}/../../../include/LightLog" ABSOLUTE)
+get_filename_component(LIGHTLOG_INCLUDE_DIR "${LIGHTLOG_DIR}/../../../include" ABSOLUTE)
